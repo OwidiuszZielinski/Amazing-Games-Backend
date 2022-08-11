61533adde7e4470b59b29ef2f20cf8cef6080a28
Owidiusz Zieliński <owi19955@gmail.com>
2022-08-11 19:42:32 +0200

    initial

---
+++ src/main/java/com/example/amazinggamesbackend/AmazingGamesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.amazinggamesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingGamesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingGamesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amazinggamesbackend/AmazingGamesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazinggamesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingGamesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
