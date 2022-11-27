6685c82d18d5bcedd9855fe140c6f5ede985a3cc
Owidiusz Zieliński <owi19955@gmail.com>
2022-11-27 19:37:09 +0100

    add dependency

---
+++ src/test/java/com/example/amazinggamesbackend/core/cart/CartServiceTests.java
@@ -0,0 +1,47 @@
+package com.example.amazinggamesbackend.core.cart;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartServiceTests {
+
+    @BeforeEach
+    void setUp() {
+
+
+    }
+
+    @Test
+    void getCartByUserId() {
+    }
+
+    @Test
+    void checkCartExists() {
+    }
+
+    @Test
+    void addGameToCart() {
+    }
+
+    @Test
+    void deleteGameFromCart() {
+    }
+
+    @Test
+    void cleanCart() {
+    }
+
+    @Test
+    void createCartForUser() {
+    }
+
+    @Test
+    void increaseGameQty() {
+    }
+
+    @Test
+    void getUserCart() {
+    }
+}
