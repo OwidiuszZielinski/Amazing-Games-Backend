d85756c3273c0f536b035a80c8404b5a63d9963f
Owidiusz Zieliński <owi19955@gmail.com>
2022-12-01 03:58:48 +0100

    Cart Service Test with OK params

--- src/main/java/com/example/amazinggamesbackend/core/cart/model/Cart.java
+++ src/main/java/com/example/amazinggamesbackend/core/cart/model/Cart.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Cart {
 
     @Id
--- src/test/java/com/example/amazinggamesbackend/core/cart/CartServiceTests.java
+++ src/test/java/com/example/amazinggamesbackend/core/cart/CartServiceTests.java
@@ -4,42 +4,41 @@ import com.example.amazinggamesbackend.core.cart.dto.CartDTO;
 import com.example.amazinggamesbackend.core.cart.model.Cart;
 import com.example.amazinggamesbackend.core.cart.model.CartDetail;
 import com.example.amazinggamesbackend.core.games.GameService;
-import com.example.amazinggamesbackend.core.games.dto.GameDTO;
-import com.example.amazinggamesbackend.core.games.exceptions.GameNotFound;
 import com.example.amazinggamesbackend.core.games.model.Game;
+import com.example.amazinggamesbackend.core.users.UserRepository;
 import com.example.amazinggamesbackend.core.users.UserService;
 import com.example.amazinggamesbackend.core.users.model.User;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class CartServiceTests {
 
 
+    private CartService cartService;
 
-    private CartService cartService ;
-
-    private CartRepository cartRepository ;
+    private CartRepository cartRepository;
+    private UserRepository userRepository;
     private UserService userService;
     private GameService gameService;
-
 
 
     @BeforeEach
     public void setUp() {
         cartRepository = mock(CartRepository.class);
+        userRepository = mock(UserRepository.class);
         userService = mock(UserService.class);
         gameService = mock(GameService.class);
-        cartService = new CartService(userService,cartRepository,gameService);
+        cartService = new CartService(userService ,cartRepository ,gameService);
     }
 
 
@@ -58,15 +57,13 @@ class CartServiceTests {
         CartDTO cartByUserId = cartService.getCartByUserId(user.getId());
         //then
         //sprawdzamy czy metoda na repo wywolala sie raz przy wywolaniu cartService.getCartByUserId
-        verify(cartRepository,times(1)).findByUserId(1);
-
-
+        verify(cartRepository ,times(1)).findByUserId(1);
 
 
     }
 
     @Test
-    void should_checkCartExists() {
+    void should_checkCartExists_params_OK() {
         //given
         Cart cartExample = new Cart();
         User user = new User();
@@ -76,21 +73,21 @@ class CartServiceTests {
         //when
         boolean checkCartExists = cartService.checkCartExists(user.getId());
         //then
-        verify(cartRepository,times(1)).findByUserId(user.getId());
+        verify(cartRepository ,times(1)).findByUserId(user.getId());
         assertTrue(checkCartExists);
 
     }
 
 
     @Test
-    void should_addGameToCart_with_good_parameters() {
+    void should_addGameToCart_with_params_OK() {
         //given
         Game givenGame = Game.builder().id(1).build();
         User givenUser = new User();
         givenUser.setId(1);
         Cart givenCart = Cart.builder()
                 .user(givenUser)
-                .cartDetails(List.of(new CartDetail(givenGame,1)))
+                .cartDetails(List.of(new CartDetail(givenGame ,1)))
                 .build();
 
         when(cartRepository.findByUserId(givenUser.getId())).thenReturn(Optional.of(givenCart));
@@ -98,33 +95,99 @@ class CartServiceTests {
         //when
         cartService.addGameToCart(givenUser.getId() ,givenGame.getId());
         //then
-        verify(cartRepository,times(1)).save(givenCart);
+        verify(cartRepository ,times(1)).save(givenCart);
 
 
+    }
+
+    @Test
+    void should_deleteGameFromCart_params_OK() {
+        //given
+        List<CartDetail> cartElements = new ArrayList<>();
+        Game givenGame = Game.builder().id(1).build();
+        User givenUser = new User();
+        givenUser.setId(1);
+        cartElements.add(new CartDetail(givenGame ,1));
+        Cart givenCart = Cart.builder()
+                .user(givenUser)
+                .cartDetails(cartElements)
+                .build();
+
+        when(cartRepository.findByUserId(givenUser.getId())).thenReturn(Optional.of(givenCart));
+        //when
+        cartService.deleteGameFromCart(givenUser.getId() ,givenGame.getId());
+        //then
+        verify(cartRepository ,times(1)).save(givenCart);
+        assertTrue(cartElements.isEmpty());
+
+
+    }
+
+    @Test
+    void should_clearCart_params_OK() {
+        //given
+        List<CartDetail> list = new ArrayList<>();
+        Game givenGame = Game.builder().id(1).build();
+        Game givenGameTwo = Game.builder().id(2).build();
+        User givenUser = new User();
+        givenUser.setId(1);
+        list.add(new CartDetail(givenGame ,1));
+        list.add(new CartDetail(givenGameTwo ,1));
+        Cart givenCart = Cart.builder()
+                .user(givenUser)
+                .cartDetails(list)
+                .build();
+
+        when(cartRepository.findByUserId(givenUser.getId())).thenReturn(Optional.of(givenCart));
+        //when
+        cartService.cleanCart(givenUser.getId());
+        //then
+        verify(cartRepository ,times(1)).save(givenCart);
+        boolean checkCart = givenCart.getCartDetails().isEmpty();
+        assertTrue(checkCart);
+
+    }
+
+    @Test
+    void should_createCartForUser_params_OK() {
+
+        //when
+        cartService.createCartForUser(Mockito.anyInt());
+        //then
+        verify(cartRepository ,times(1)).save(Mockito.any(Cart.class));
 
 
 
     }
 
     @Test
-    void deleteGameFromCart() {
+    void should_increase_game_quantity_1pcs_in_cart_params_OK() {
+        //given
+        final Cart givenCart = new Cart();
+        final Game givenGame = Game.builder().id(1).build();
+        givenCart.setCartDetails(List.of(new CartDetail(givenGame ,1)));
 
+        //when
+        cartService.increaseGameQty(givenCart,givenGame.getId());
+        //then
+        int quantity = givenCart.getCartDetails().get(0).getQuantity();
+        Assertions.assertEquals(quantity ,2);
 
     }
 
     @Test
-    void cleanCart() {
-    }
-
-    @Test
-    void createCartForUser() {
-    }
-
-    @Test
-    void increaseGameQty() {
-    }
-
-    @Test
-    void getUserCart() {
+    void should_getUserCart_params_OK() {
+        User givenUser = new User();
+        givenUser.setId(1);
+        Cart givenCart = Cart.builder().user(givenUser).build();
+        //given
+        when(cartRepository.findByUserId(Mockito.anyInt())).thenReturn(Optional.of(givenCart));
+        //when
+        final Cart userCart = cartService.getUserCart(1);
+        //then
+        verify(cartRepository,times(1)).findByUserId(1);
+        final int expectedUserFromCart = userCart.getUser().getId();
+        final int givenUserFromCart = givenUser.getId();
+        Assertions.assertEquals(expectedUserFromCart ,givenUserFromCart);
     }
 }
