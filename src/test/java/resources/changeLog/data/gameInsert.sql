0ac4e6e79ad212cfaedac5d881cf588764dce409
Owidiusz Zieliński <owi19955@gmail.com>
2023-02-06 23:14:58 +0100

    init

---
+++ src/test/java/resources/changeLog/data/gameInsert.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+--changeset owi:2
+INSERT INTO game(id, availability, description, price, rating, title, "type")VALUES(1, '1', 'test', 0, 0, 'exampleOne', 'example');
+INSERT INTO game(id, availability, description, price, rating, title, "type")VALUES(2, '1', 'test', 1, 1, 'exampleTwo', 'example');
+INSERT INTO game(id, availability, description, price, rating, title, "type")VALUES(3, '1', 'test', 2, 2, 'exampleThree', 'example');
+INSERT INTO game(id, availability, description, price, rating, title, "type")VALUES(4, '1', 'test', 3, 3, 'exampleFour', 'example');
