850acfb9c8b5e0a9e0729b64315ab58ebf6440bd
Owidiusz Zieliński <owi19955@gmail.com>
2023-02-07 02:48:41 +0100

    add aop logger

---
+++ src/main/java/com/example/amazinggamesbackend/aop/Logger.java
@@ -0,0 +1,28 @@
+package com.example.amazinggamesbackend.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+@EnableAspectJAutoProxy
+public class Logger {
+    @Pointcut("execution(* com.example.amazinggamesbackend.*(..))")
+    private void anyPublicMethod(){
+
+    }
+    @Around("execution(* com.example.amazinggamesbackend.app.QRController.*(..))")
+    public Object arroundController(ProceedingJoinPoint point) throws Throwable {
+        Object proceed = point.proceed();
+        long start = System.nanoTime();
+        log.info("::Time:: = " + (System.nanoTime() - start) + " nanosec" + " QR HAS BEEN DOWNLOADED");
+        return proceed;
+    }
+
+}
